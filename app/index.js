de3efa3153bb25e85f651baaca4f5d040d7fda35
codecalypso <smullins10@gmail.com>
2024-12-09 13:04:34 -0600

    create onboarding screen

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import {Text, View, Image, ScrollView} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import CustomButton from '../components/CustomButton';
+import { Redirect, router } from "expo-router";
 import { images } from "../constants";
-// import '../app/global.css';
+import { StatusBar } from "expo-status-bar";
+
 export default function App(){
     return(
       <SafeAreaView className="bg-primary h-full">
         <ScrollView contentContainerStyle={{height: '100%'}}>
-          <View className="w-full h-full px-4">
+          <View className="w-full flex justify-center items-center min-h-[85vh] px-4">
             <Image
               source={images.logo}
               className="w-[130px] h-[84px]"
@@ -29,9 +32,20 @@ export default function App(){
                   className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
                   resizeMode="contain"
                 />
+          
             </View>
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+              Where Creativity Meets Innovation: Embark on a Journey of Limitless
+              Exploration with Aora
+            </Text>
+            <CustomButton
+              title="Continue with Email"
+              handlePress={() => router.push("/sign-in")}
+              containerStyles="w-full mt-7"
+            />
           </View>
         </ScrollView>
+        <StatusBar backgroundColor="#161622" style="light" />
       </SafeAreaView>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    // './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    '*.{js,ts,jsx,tsx,mdx}'
+    // '*.{js,ts,jsx,tsx,mdx}'
   ],
   presets: [require("nativewind/preset")],
   theme: {
